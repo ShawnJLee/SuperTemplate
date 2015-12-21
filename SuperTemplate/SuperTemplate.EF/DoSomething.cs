47390a98a23b8ac1ffd3e6ddb465cada29c4ceb9
shawnjlee <slee@quicksolutions.com>
2015-12-21 13:47:57 -0500

    StructureMap (part 3 actually now)

---
+++ SuperTemplate/SuperTemplate.EF/DoSomething.cs
@@ -0,0 +1,11 @@
+﻿using SuperTemplate.Core;
+namespace SuperTemplate.EF
+{
+    public class DoSomething : IDoSomething
+    {
+        public string GiveMeAString()
+        {
+            return "I'm a string yay!";
+        }
+    }
+}
--- SuperTemplate/SuperTemplate.SM/Properties/AssemblyInfo.cs
+++ SuperTemplate/SuperTemplate.SM/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("SuperTemplate.StructureMap")]
+[assembly: AssemblyTitle("SuperTemplate.SM")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("SuperTemplate.StructureMap")]
+[assembly: AssemblyProduct("SuperTemplate.SM")]
 [assembly: AssemblyCopyright("Copyright ©  2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
